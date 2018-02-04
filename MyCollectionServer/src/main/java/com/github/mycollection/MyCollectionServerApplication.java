7e1e53b702d5ca09e9f133895e9e90bac9c96567
jebbard <jensebert@gmx.net>
2018-02-04 19:12:37 +0100

    INFRA: Newly created server code project, empty but tweaked to be buildable with Java 9 (mvn clean install works - requires installed

---
+++ MyCollectionServer/src/main/java/com/github/mycollection/MyCollectionServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.mycollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCollectionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCollectionServerApplication.class, args);
+	}
+}
